3f2299945d5212a05e854c2309cc9dab0539a588
THAYSZ <thainacristina.sz07@gmail.com>
2024-02-29 16:21:03 -0300

    Personalização do componente Patrimônio e tela Inventários

--- Inventário Senai/Screens/Inventarios.tsx
+++ Inventário Senai/Screens/Inventarios.tsx
@@ -1,13 +1,32 @@
 import { View, Text, StyleSheet } from "react-native";
 import Nav from "../components/Nav";
-
+import Patrimonio from "../components/Patrimonio";
+import { StatusBar } from "expo-status-bar";
 
 function Inventarios(){
     return(
         <View>
+            <StatusBar style="light"/>
             <Nav icon="menu" icon2="search"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/><Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+            <Patrimonio id={956389} categoria="CADEIRA SENAI"/>
+        
         </View>
     )
 }
+const styles = StyleSheet.create({
 
+})
 export default Inventarios
--- Inventário Senai/components/Patrimonio.tsx
+++ Inventário Senai/components/Patrimonio.tsx
@@ -1,15 +1,38 @@
 import { View, Text, StyleSheet } from "react-native";
 
 type PatrimonioProps = {
-    number: number,
+    id: number,
     categoria: string
 }
 
-function Patrimonio({number, categoria}: PatrimonioProps){
+function Patrimonio({id, categoria}: PatrimonioProps){
     return(
-        <View>
-            <Text>{number}</Text>
-            <Text>{categoria}</Text>
+        <View style={styles.patrimonio}>
+            <Text style={styles.id}>{id}</Text>
+            <Text style={styles.categoria}>{categoria}</Text>
         </View>
     )
+    
 }
+
+const styles = StyleSheet.create({
+    id:{
+        fontSize: 16,
+        fontWeight: '600'
+    },
+    categoria:{
+        fontSize: 14,
+        color: '#595959'
+    },
+    patrimonio:{
+        borderColor: 'F5F5F5',
+        borderTopWidth: 1,
+        gap: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        width: '100%',
+        height: 66
+    }
+})
+
+export default Patrimonio
